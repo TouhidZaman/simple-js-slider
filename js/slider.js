5d5684b8ce59dc9ca21bda91cc410cbc35aae703
Muhammad Touhiduzzaman <touhid4bd@gmail.com>
2022-03-13 16:02:25 +0600

    slider dynamic json data and slider functionality added

--- js/slider.js
+++ js/slider.js
@@ -1,1 +1,29 @@
-console.log("JS Connected")
+//To display slider images to UI
+const showSlider =  (images) => {
+    let sliderIndex = 0;
+    let isSliderEnabled = false;
+    const sliderContainer = document.getElementById('slider');
+    const sliderImg =  document.getElementById("slider-img");
+
+    //This block of code bellow will dynamically set the slider image url
+    setInterval(() => {
+        if(sliderIndex >= images.length) {
+            sliderIndex = 0;
+        }
+        sliderImg.setAttribute('src', images[sliderIndex].imgUrl)
+        sliderIndex++;
+        if(!isSliderEnabled) {
+            sliderContainer.classList.remove('d-none');
+            isSliderEnabled = true;
+        } 
+    }, 2000);
+}
+
+// Loading data 
+const loadSliderImages = async () => {
+    let imgData = await fetch('../data/slider-images.json');
+    sliderImages = await imgData.json();
+    showSlider(sliderImages);
+}
+
+loadSliderImages();
