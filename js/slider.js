0b45e155fa8ae497c7d1d8e855766f40fde6cf93
Muhammad Touhiduzzaman <touhid4bd@gmail.com>
2022-03-13 20:13:06 +0600

    Slider JSON data updated and url adjusted

--- js/slider.js
+++ js/slider.js
@@ -1,27 +1,27 @@
 //To display slider images to UI
-const showSlider =  (images) => {
+const showSlider = (images) => {
     let sliderIndex = 0;
     let isSliderEnabled = false;
     const sliderContainer = document.getElementById('slider');
-    const sliderImg =  document.getElementById("slider-img");
+    const sliderImg = document.getElementById("slider-img");
 
     //This block of code bellow will dynamically set the slider image url
     setInterval(() => {
-        if(sliderIndex >= images.length) {
+        if (sliderIndex >= images.length) {
             sliderIndex = 0;
         }
         sliderImg.setAttribute('src', images[sliderIndex].imgUrl)
         sliderIndex++;
-        if(!isSliderEnabled) {
+        if (!isSliderEnabled) {
             sliderContainer.classList.remove('d-none');
             isSliderEnabled = true;
-        } 
+        }
     }, 2000);
 }
 
 // Loading data 
-const loadSliderImages = async () => {
-    let imgData = await fetch('../data/slider-images.json');
+const loadSliderImages = async() => {
+    let imgData = await fetch('data/slider-images.json');
     sliderImages = await imgData.json();
     showSlider(sliderImages);
 }
